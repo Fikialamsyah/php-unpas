851284154f5756f9b7377d78feebd77d771389d0
fiki <fiki@pop-os.localdomain>
2021-03-22 17:28:01 +0700

    add pertemuan 11

---
+++ pertemuan 11/ubah.php
@@ -0,0 +1,69 @@
+<?php
+include 'functions.php';
+
+// ambil data id
+$id = $_GET['id'];
+
+// query data mahasiswa berdasarkan id
+$mhs = query("SELECT * FROM mahasiswa WHERE id = $id")[0];
+
+// cek apakah tombol submit sudah ditekan atau belum
+if(isset($_POST['submit'])){
+
+    // cek data berhasil data diubah atau tidak
+    if(ubah($_POST) > 0){
+        echo "
+        <script>
+            alert('data berhasil diubah');
+            document.location.href = 'index.php';
+        </script>";
+    } else {
+        echo "
+        <script>
+            alert('data gagal diubah');
+            document.location.href = 'index.php';
+        </script>";
+    }
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Ubah Data Mahasiswa</h1>
+    <form action="" method="POST">
+        <ul>
+                <input type="hidden" name="id" id="nim" required" value="<?= $mhs['id']; ?>">
+            <li>
+                <label for="nim">NIM</label>
+                <input type="text" name="nim" id="nim" required" value="<?= $mhs['nim']; ?>">
+            </li>
+            <li>
+                <label for="nama">Nama</label>
+                <input type="text" name="nama" id="nama" required value="<?= $mhs['nama']; ?>">
+            </li>
+            <li>
+                <label for="email">Email</label>
+                <input type="text" name="email" id="email" required value="<?= $mhs['email']; ?>">
+            </li>
+            <li>
+                <label for="jurusan">Jurusan</label>
+                <input type="text" name="jurusan" id="jurusan" required value="<?= $mhs['jurusan']; ?>"?
+            </li>
+            <li>
+                <label for="gambar">Gambar</label>
+                <input type="text" name="gambar" id="gambar" required value="<?= $mhs['gambar']; ?>">
+            </li>
+            <li><button type="submit" name="submit">Ubah Data !</button></li>
+        </ul>
+    </form>
+    <a href="index.php">kembali</a>
+</body>
+</html>
